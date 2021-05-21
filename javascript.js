d3b27e23fa9fdbbb285a856891033b650d18be4b
Jose Jacsan <jjacsan@gmail.com>
2021-05-21 13:31:31 -0300

    Correções feitas e link atualizado

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
 function calcular() {
-    var camp1 = document.getElementById('campo1');
-    var camp2 = document.getElementById('campo2');
+    var campo1 = document.getElementById('campo1');
+    var campo2 = document.getElementById('campo2');
     var res = document.getElementById('res');
     var res2 = document.getElementById('res2');
 
-    var num1 = Number(camp1.value);
-    var num2 = Number(camp2.value);
+    var num1 = Number(campo1.value);
+    var num2 = Number(campo2.value);
 
     var calcular = num1 / 10;
-    var calcular2 = (num1 - calcular) / num2;
+    var calcular2 = (num1 - calcular) / (100 / num2);
 
     res.innerHTML = `Seu dízimo: R$ ${calcular.toFixed(2)}`;
     res2.innerHTML = `Sua oferta: R$ ${calcular2.toFixed(2)}`;
